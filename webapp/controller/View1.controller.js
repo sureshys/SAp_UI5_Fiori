1a72e8b5579e9642641fa4de02d4e0980d7717d3
Suresh Shindhe <sureshshindhe6666@gmail.com>
2024-09-29 08:40:26 +0000

    added read all method

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,25 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("odatacrud.controller.View1", {
-        onInit: function () {
-
-        }
+        return Controller.extend("odatacrud.controller.View1", {
+            onInit: function () {
+                this.onReadAll();
+            },
+            onReadAll: function () {
+                var that = this;
+                var oModel = this.getOwnerComponent().getModel();
+                oModel.read("/Products", {
+                    success: function (oData) {
+                        console.log(oData);
+                        var jModel = new sap.ui.model.json.JSONModel(oData);
+                        that.getView().byId("idProducts").setModel(jModel);
+                    }, error: function (oError) { 
+                        console.log(oError);
+                     }
+                })
+            }
+        });
     });
-});
